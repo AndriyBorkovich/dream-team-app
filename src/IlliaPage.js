95eb459049b67be2e2fe50119615cadcb16b43de
Illia Dudchyk <illia.dudchyk.pz.2021@lpnu.ua>
2023-12-05 20:55:33 +0200

    Start work

---
+++ src/IlliaPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import IlliaPhoto from './images/illia-anime.jpg'
+ export function MainContent() {
+    return (
+        <div>
+        <h1>My dream team</h1>
+        <div className="illia-block">
+            <p className="name illiaN">Illia Dudchyk</p>
+            <div><p className="position illiaP">Developer</p></div>
+            <img className="illia-photo" src = {IlliaPhoto}></img>
+        </div>
+        </div>
+    )
+    
+}
