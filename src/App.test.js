6230521b955b0b85f020f6c3db7a1f92c87c88c4
Andriy Borkovich <borkovich25andri@gmail.com>
2023-12-05 21:33:04 +0200

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  test('renders App component', () => {
+    const { getByText } = render(<App />);
+
+    const headerElement = getByText(/Our dream team/i);
+    expect(headerElement).toBeInTheDocument();
+
+  });
 });
