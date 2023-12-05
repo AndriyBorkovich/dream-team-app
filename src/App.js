a26f6c92de57f524071ddea6ed3093db5194e853
Illia Dudchyk <illia.dudchyk.pz.2021@lpnu.ua>
2023-12-05 21:49:28 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {MainContent} from './IlliaPage';
 import MyCard from './CardInfo.jsx'
 import IlliaPhoto from './images/illia-anime.jpg'
 
 function App() {
-  const myName = 'Your Name';
-  const additionalInfo = 'Additional Information about you.';
 
   return (
       <div>
